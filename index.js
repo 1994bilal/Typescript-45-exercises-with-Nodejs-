983995022d0e8089a01b5f9c4a0eea1fedc849df
1994bilal <163435734+1994bilal@users.noreply.github.com>
2024-06-15 16:04:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//exercise # 2 :
+//Personal Message: Store a person’s name in a variable, and print a message to
+//that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let person_Name = " Bilal ";
+let message = `hello ${person_Name} , would you like to learn some typescript today?`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,9 @@
+                  //exercise # 2 :
+
+//Personal Message: Store a person’s name in a variable, and print a message to
+//that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+let person_Name:string= " Bilal ";
+let message:string= `hello ${person_Name} , would you like to learn some typescript today?`;
+console.log(message);
